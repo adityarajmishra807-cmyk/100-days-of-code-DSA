6a4d7c83523b37931956df2eb6e84ea63d1b0c5d
adityarajmishra807-cmyk <adityarajmishra807@gmail.com>
2026-02-03 15:10:13 +0530

    Create Q3.c

---
+++ Q3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int n, k, i;
+    int comparisons = 0;
+    int foundIndex = -1;
+
+    // Input array size
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input array elements
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Input key to search
+    scanf("%d", &k);
+
+    // Linear search
+    for (i = 0; i < n; i++) {
+        comparisons++;
+        if (arr[i] == k) {
+            foundIndex = i;
+            break;
+        }
+    }
+    if (foundIndex != -1) {
+        printf("Found at index %d\n", foundIndex);
+    } else {
+        printf("Not Found\n");
+    }
+
+    printf("Comparisons = %d", comparisons);
+
+    return 0;
+}
