f27ec5761b4fc44aa8d1da3de66563e903d17b60
adityarajmishra807-cmyk <adityarajmishra807@gmail.com>
2026-02-08 22:25:59 +0530

    Implement logic to remove duplicates from array
    Fix input handling and output of unique elements.

--- Q6.c
+++ Q6.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    if (n == 0) {
+        return 0;
+    }
+
+    int j = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (a[i] != a[j]) {
+            j++;
+            a[j] = a[i];
+        }
+    }
+
+    for (int i = 0; i <= j; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
 
